03443c17c96622759d00be6c8d71243c22ae5577
Kralickova, Eva <eva.kralickova@oracle.com>
2022-10-21 16:28:44 +0200

    Ukol 1

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log('funguju!');
+const Task = (props) => {
+    const { name, due, done } = props;
+
+    return `
+      <div class=${done === true ? "" : "todo__tasks"}>
+        <div class="task">
+        <div class="task__body">
+          <div class="task__name">${name}</div>
+          <div class="task__due">${due}</div>
+        </div>
+        <div class="task__done">${done === true ? "" : "✓"}</div>
+      </div>
+   `;
+};
+
+const renderTasks = (task) => {
+    const tasksList = document.querySelector('.todo__tasks');
+    tasksList.innerHTML = task
+        .map((task) => Task(task))
+        .join('');
+};
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks')
+    .then((response) => response.json())
+    .then(renderTasks);
+
+const checkbox = document.querySelector('#checkbox-undone');
+checkbox.addEventListener('change', (event) => {
+    event.preventDefault();
+    let done = '';
+    if (event.target.checked) {
+        done = '?done=true';
+    }
+    fetch('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks' + done)
+        .then((response) => response.json())
+        .then(renderTasks)
+}
+);
